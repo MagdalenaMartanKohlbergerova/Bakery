5a9c4396dc64219c176b9fdc21afd30403fb6adb
Magdaléna Martan Kohlbergerová <magdalenaa.martankohlbergerova@gmail.com>
2023-04-17 16:04:32 +0200

    product slideshow

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ let search = document.querySelector('.search');
 document.querySelector('#search').onclick=() =>{
     search.classList.toggle('active')
 }
+
+var swiper = new Swiper(".product-row", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay:{
+        delay: 9500,
+        disableOnInteraction: false, 
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+      },
+    },
+  });
