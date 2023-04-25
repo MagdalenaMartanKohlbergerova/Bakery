c00c23e07d5678c420be581fc669d6d1d1ed2d7f
Magdaléna Martan Kohlbergerová <magdalenaa.martankohlbergerova@gmail.com>
2023-04-25 13:27:49 +0200

    Client review

--- index.js
+++ index.js
@@ -58,3 +58,28 @@ var swiper = new Swiper(".product-row", {
       },
     },
   });
+
+  var swiper = new Swiper(".review-row", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay:{
+        delay: 9500,
+        disableOnInteraction: false, 
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+      },
+    },
+  });
